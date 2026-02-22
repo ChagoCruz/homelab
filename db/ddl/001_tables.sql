e96f1efe6e8980a9e4e71d122b45155bc5e15a9a
ChagoCruz <ramosrecording@gmail.com>
2026-02-21 22:55:34 -0500

    add rest of tables to tables script

--- db/ddl/001_tables.sql
+++ db/ddl/001_tables.sql
@@ -1,3 +1,71 @@
+CREATE TABLE IF NOT EXISTS bills
+(
+    id integer NOT NULL DEFAULT nextval('bills_id_seq'::regclass),
+    name text COLLATE pg_catalog."default" NOT NULL,
+    amount numeric(10,2) NOT NULL,
+    due_date date NOT NULL,
+    paid boolean DEFAULT false,
+    paid_date date,
+    CONSTRAINT bills_pkey PRIMARY KEY (id)
+)
+
+CREATE TABLE IF NOT EXISTS car_mileage
+(
+    id integer NOT NULL DEFAULT nextval('car_mileage_id_seq'::regclass),
+    log_date date NOT NULL,
+    odometer integer NOT NULL,
+    CONSTRAINT car_mileage_pkey PRIMARY KEY (id)
+)
+
+CREATE TABLE IF NOT EXISTS expenses
+(
+    id integer NOT NULL DEFAULT nextval('expenses_id_seq'::regclass),
+    amount numeric(10,2) NOT NULL,
+    description text COLLATE pg_catalog."default" NOT NULL,
+    spent_date date NOT NULL,
+    category text COLLATE pg_catalog."default",
+    CONSTRAINT expenses_pkey PRIMARY KEY (id)
+)
+
+CREATE TABLE IF NOT EXISTS income
+(
+    id integer NOT NULL DEFAULT nextval('income_id_seq'::regclass),
+    source text COLLATE pg_catalog."default" NOT NULL,
+    amount numeric(10,2) NOT NULL,
+    received_date date NOT NULL,
+    notes text COLLATE pg_catalog."default",
+    CONSTRAINT income_pkey PRIMARY KEY (id)
+)
+
+CREATE TABLE IF NOT EXISTS weight
+(
+    id integer NOT NULL DEFAULT nextval('weight_id_seq'::regclass),
+    entry_date date NOT NULL,
+    weight numeric(5,2) NOT NULL,
+    CONSTRAINT weight_pkey PRIMARY KEY (id)
+)
+
+CREATE TABLE journal (
+    id SERIAL PRIMARY KEY,
+    entry_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    content TEXT NOT NULL
+);
+
+CREATE TABLE diet (
+    id SERIAL PRIMARY KEY,
+    log_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    meal TEXT NOT NULL,       -- e.g., "Breakfast", "Lunch", "Dinner", "Snack"
+    food TEXT NOT NULL,       -- description of what you ate
+    calories INTEGER NOT NULL -- estimated calories for that food
+);
+
+CREATE TABLE workout (
+	id SERIAL PRIMARY KEY,
+	workout_date DATE NOT NULL DEFAULT CURRENT_DATE,
+	workout TEXT NOT NULL,
+	calories_burnt INTEGER NOT NULL
+);
+
 CREATE TABLE tarot_cards (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
